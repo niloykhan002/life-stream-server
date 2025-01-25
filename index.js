812423973c79d02dcca3ff4721af6ace38fc89d5
niloykhan002 <barkatificbankbd@gmail.com>
2025-01-25 20:37:58 +0600

    verify token middleware added

--- index.js
+++ index.js
@@ -33,20 +33,20 @@ async function run() {
       res.send({ token });
     });
 
-    // // middlewares
-    // const verifyToken = (req, res, next) => {
-    //   if (!req.headers.authorization) {
-    //     return res.status(401).send({ message: "unauthorized access" });
-    //   }
-    //   const token = req.headers.authorization.split(" ")[1];
-    //   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    //     if (err) {
-    //       return res.status(401).send({ message: "unauthorized access" });
-    //     }
-    //     req.decoded = decoded;
-    //     next();
-    //   });
-    // };
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
 
     // Users api
     app.post("/users", async (req, res) => {
