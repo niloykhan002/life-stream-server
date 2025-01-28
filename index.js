fb5d714d4646cd4223d09d5789ed7536fade0c57
niloykhan002 <barkatificbankbd@gmail.com>
2025-01-28 20:00:52 +0600

    get api for blogs added

--- index.js
+++ index.js
@@ -264,6 +264,11 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/blogs", async (req, res) => {
+      const result = await blogsCollection.find().toArray();
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
